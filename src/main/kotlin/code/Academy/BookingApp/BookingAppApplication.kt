004027846332729c67a72b5da46c3d1b389cf14d
Ramonas <sanomaras23@gmail.com>
2022-12-12 18:10:55 +0200

    First commit

---
+++ src/main/kotlin/code/Academy/BookingApp/BookingAppApplication.kt
@@ -0,0 +1,11 @@
+package code.Academy.BookingApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookingAppApplication>(*args)
+}
---
+++ src/test/kotlin/code/Academy/BookingApp/BookingAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package code.Academy.BookingApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
